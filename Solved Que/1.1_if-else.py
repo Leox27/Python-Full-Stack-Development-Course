e48ae14236f8c4ec6ce715caeab7937098561b48
Leox27 <mayurx27@gmail.com>
2026-02-26 06:37:06 +0530

    Solved the HackerRank Que 1.1_if-else

---
+++ Solved Que/1.1_if-else.py
@@ -0,0 +1,63 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2 != 0:
+        print("Weird")
+    elif 2<=n<=5:
+        print("Not Weird")
+    elif 6<=n<=20:
+        print("Weird")
+    elif n>20:
+        print("Not Weird")
+        
+'''
+https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+Task
+
+Given an integer, n, perform the following conditional actions:
+. If n is odd, print Weird
+. If n is even and in the inclusive range of 2 to 5, print Not Weird
+. If n is even and in the inclusive range of 6 to 20, print Weird
+. If n is even and greater than 20, print Not Weird
+
+Input Format
+A single line containing a positive integer, n.
+
+Constraints
+. 1<n ≤ 100
+
+Output Format
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+Sample Input O
+3
+
+Sample Output O
+Weird
+
+Explanation O
+n= 3
+
+n is odd and odd numbers are weird, so print Weird.
+
+Sample Input 1
+24
+
+Sample Output 1
+Not Weird
+
+Explanation 1
+n=24
+
+n > 20 and n is even, so it is not weird.
+'''
