587a72e6b22ea39def119d72e93095dff8b0a2af
Leox27 <mayurx27@gmail.com>
2026-03-23 18:43:33 +0530

    Understood about the Read-operation in File Handling

---
+++ Advanced Python/09_File_Handling/03_read.py
@@ -0,0 +1,12 @@
+###
+
+a = open("X:/GitHub/QSpider/Python Full Stack Development/Advanced Python/09_File_Handling/writelines.txt", 'r')
+b = a.read()
+print(b)
+'''
+>>>
+I am Leo
+I am a student
+I am learning Python
+'''
+# From the writelines.txt file, we have read the data using read() method and stored it in variable b and then printed it.
