7c9101179b217682a034cf1afb5c471d1bc34e2d
Leox27 <mayurx27@gmail.com>
2026-03-24 22:30:14 +0530

    Learned about the json-parsing in File Handling

---
+++ Advanced Python/09_File_Handling/json Parsing/json-parsing.py
@@ -0,0 +1,28 @@
+
+'''
+#Sender:
+Take one list of numbers from 1 to 10 convert into string form check with the tyow of the collection befire and after the collectuin create one text file and write this data into the same file. 
+'''
+
+# Sender:
+import json
+data = [x for x in range(1, 11)]
+b = json.dumps(data)
+print(data, type(data))
+print(b, type(b))
+
+with open(r"X:\GitHub\QSpider\Python Full Stack Development\Advanced Python\09_File_Handling\json Parsing\parsing-by-json.txt", 'w') as a:
+    a = a.write(b)
+
+'''
+#Receiver:
+Read the file and then convert it to ita original form after doing the conversion again check the type of data
+'''
+
+#Receiver:
+with open(r"X:\GitHub\QSpider\Python Full Stack Development\Advanced Python\09_File_Handling\json Parsing\parsing-by-json.txt", 'r') as a:
+    a = a.read()
+    b = json.loads(a)
+    print(a, type(a))
+    print(b, type(b))
+    
