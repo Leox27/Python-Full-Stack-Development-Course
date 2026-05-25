f3175a4da3bba1ef6243ed600258875224313234
Leox27 <mayurx27@gmail.com>
2026-05-25 13:27:06 +0530

    Solved Que2: finding leader elements in array

--- Practice Question/Interview_Questions/01_Question.py
+++ Practice Question/Interview_Questions/01_Question.py
@@ -51,3 +51,51 @@ print(rotate([1, 2, 3, 4, 5], 'left', 2))
 [3, 4, 5, 1, 2]
 '''
 
+
+'''
+Question 2:
+Find all the leaders in an array.
+	* A leader is an element that is greater than or equal to all the elements to its right.
+	* You are given an array of integers.
+	* Traverse the array to identify all such leader elements.
+	* A leader should satisfy the condition:
+	* It must be greater than or equal to every element on its right side.
+	* Store all the leader elements in a list and display the result.
+
+Input:
+arr = [9, 10, 2, 4, 8, 5]
+
+Output:
+[10, 8, 5]
+
+Input:
+arr = [16, 17, 4, 3, 5, 2]
+
+Output:
+[17, 5, 2]
+
+Constraints:
+	* Use only loops for traversal
+	* Do not use unnecessary extra variables like flags
+	* Avoid using built-in functions for direct computation
+	* The solution should correctly check all elements to the right of each element
+'''
+
+def Leaders(arr: List[int]) -> List[int]:
+    o_list = []
+    for i in range(len(arr)):
+        count = 0
+        for k in range(i+1, len(arr)):
+            if arr[k] > arr[i]:
+                count += 1
+        if count == 0:
+            o_list.append(arr[i])
+    return o_list
+print(Leaders([9, 10, 2, 4, 8, 5]))
+print(Leaders([16, 17, 4, 3, 5, 2]))
+'''
+>>>
+[10, 8, 5]
+[17, 5, 2]
+'''
+
